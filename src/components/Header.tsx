d45c47ba1b37f1c22a0f8bbd39bb5a78d389f4c6
amrh07@gmail.com <amrh07@gmail.com>
2023-04-13 14:29:06 -0300

    changing logo import

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./Header.module.css"
+import HeaderLogo from '../../public/assets/Logo.svg'
+
 export function Header() {
 
     return (
         <header className={styles.headerLogo}>
-            <img alt="Logo" src="../../public/assets/Logo.svg" />
+            <img alt="Logo" src={HeaderLogo} />
         </header>
 
     )
